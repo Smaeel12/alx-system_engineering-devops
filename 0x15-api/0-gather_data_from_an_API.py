9f1e9e1f7a7b830a37d75cad45293509559b3ef3
Smaeel12 <ismailboubkri2002@gmail.com>
2024-05-20 21:31:24 +0000

    0x15. API

---
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+import sys
+import requests
+
+
+def get_employee_name(user_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Failed to retrieve employee name: {response.status_code}")
+        return None
+
+    user_info = response.json()
+    return user_info['name']
+
+
+def get_employee_todo_progress(employee_id):
+    todo_url = (
+            f"https://jsonplaceholder.typicode.com/"
+            f"users/{employee_id}/todos"
+    )
+    todo_response = requests.get(todo_url)
+
+    if todo_response.status_code != 200:
+        print(f"Failed to retrieve data: {todo_response.status_code}")
+        return
+
+    todos = todo_response.json()
+    employee_name = get_employee_name(employee_id)
+    if not employee_name:
+        print("Failed to retrieve employee name.")
+        return
+
+    total_tasks = len(todos)
+    completed_tasks = sum(1 for todo in todos if todo['completed'])
+
+    print(f"Employee {employee_name} is done with tasks\
+            ({completed_tasks}/{total_tasks}):")
+    for todo in todos:
+        if todo['completed']:
+            print(f"\t{todo['title']}")
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
---
+++ 0x15-api/1-export_to_CSV.py
@@ -0,0 +1,59 @@
+#!/usr/bin/python3
+import sys
+import requests
+import csv
+
+
+def get_employee_name(user_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{user_id}"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Failed to retrieve employee name: {response.status_code}")
+        return None
+
+    user_info = response.json()
+    return user_info['name']
+
+
+def export_to_csv(employee_id, todos):
+    employee_name = get_employee_name(employee_id)
+    if not employee_name:
+        print("Failed to retrieve employee name.")
+        return
+
+    filename = f"{employee_id}.csv"
+    with open(filename, 'w', newline='') as csvfile:
+        fieldnames = ['USER_ID', 'USERNAME',
+                      'TASK_COMPLETED_STATUS', 'TASK_TITLE']
+        writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+
+        writer.writeheader()
+        for todo in todos:
+            writer.writerow({
+                'USER_ID': employee_id,
+                'USERNAME': employee_name,
+                'TASK_COMPLETED_STATUS': str(todo['completed']),
+                'TASK_TITLE': todo['title']
+            })
+
+
+def get_employee_todo_progress(employee_id):
+    url = f"https://jsonplaceholder.typicode.com/users/{employee_id}/todos"
+    response = requests.get(url)
+
+    if response.status_code != 200:
+        print(f"Failed to retrieve data: {response.status_code}")
+        return
+
+    todos = response.json()
+    export_to_csv(employee_id, todos)
+
+
+if __name__ == "__main__":
+    if len(sys.argv) != 2:
+        print("Usage: python3 1-export_to_CSV.py <employee_id>")
+        sys.exit(1)
+
+    employee_id = int(sys.argv[1])
+    get_employee_todo_progress(employee_id)
