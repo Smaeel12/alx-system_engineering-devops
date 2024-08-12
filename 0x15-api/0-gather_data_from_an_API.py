e94112cc937a771a47b2e8a80eb5faef4c0a8d2f
root <Smaeel12>
2024-08-11 17:27:11 -0700

    no way

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,30 +1,33 @@
 #!/usr/bin/python3
-"""
-Retrieves employee info using a REST API
-"""
-if __name__ == "__main__":
-    import json
-    import requests
-    from sys import argv
+""" given employee ID, returns information about his/her TODO list progress """
+import requests
+from sys import argv
 
-    id = argv[1]
-    request = requests.get(f"https://jsonplaceholder.typicode.com/users/{id}")
-    EMPLOYEE_NAME = request.json()["name"]
-    requestTasks = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos/?userId={id}"
-    )
-    TOTAL_NUMBER_OF_TASKS = len(requestTasks.json())
-    NUMBER_OF_DONE_TASKS = 0
-    TASK_TITLE = ""
-    for dictionary in requestTasks.json():
-        for key, val in dictionary.items():
-            if key == "completed" and val is True:
-                NUMBER_OF_DONE_TASKS += 1
-                TASK_TITLE += "\n"
-                TASK_TITLE += "\t "
-                TASK_TITLE += f"{dictionary['title']}"
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            EMPLOYEE_NAME, NUMBER_OF_DONE_TASKS, TOTAL_NUMBER_OF_TASKS
-        ), end="")
-    print(TASK_TITLE)
+
+if __name__ == "__main__":
+    """ main code """
+    r_info = requests.get(
+        "https://jsonplaceholder.typicode.com/todos").json()
+    r_users = requests.get(
+        "https://jsonplaceholder.typicode.com/users").json()
+    r_user_dict = list(
+        filter(
+            lambda elem: elem.get("id") == int(
+                argv[1]),
+            r_users))[0]
+    user = r_user_dict.get('name')
+    r_info_filtered = list(
+        filter(
+            lambda elem: elem.get('userId') == int(
+                argv[1]),
+            r_info))
+    done = list(
+        filter(
+            lambda elem: elem.get('completed'),
+            r_info_filtered))
+    num_task = len(done)
+    total_num_task = len(r_info_filtered)
+    print("Employee {} is done with ".format(user), end='')
+    print('tasks({}/{}):'.format(num_task, total_num_task))
+    for i in done:
+        print('\t {}'.format(i.get('title')))
