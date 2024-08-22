797628b8968b3995595607617cee9ddf9431a4e5
Smaeel12 <ismailboubkri2002@gmail.com>
2024-08-22 20:57:47 +0100

    puppet lint

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -1,5 +1,5 @@
 # A puppet script
 exec { 'fixing the security file':
-	command => "sed -i -E 's/(holberton (hard|soft) nofile )[0-9]+/1-1/' etc/security/limits.conf",
-	path    => ['/bin/', '/usr/bin/'] 
+  command => "sed -i -E 's/(holberton (hard|soft) nofile )[0-9]+/1-1/' etc/security/limits.conf",
+  path    => ['/bin/', '/usr/bin/']
 }
