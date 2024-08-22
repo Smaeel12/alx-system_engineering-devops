23814fec1f49cfd66928ad35d908222b3bd21f52
Smaeel12 <ismailboubkri2002@gmail.com>
2024-08-22 20:56:08 +0100

    task 1

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,5 @@
+# A puppet script
+exec { 'fixing the security file':
+	command => "sed -i -E 's/(holberton (hard|soft) nofile )[0-9]+/1-1/' etc/security/limits.conf",
+	path    => ['/bin/', '/usr/bin/'] 
+}
